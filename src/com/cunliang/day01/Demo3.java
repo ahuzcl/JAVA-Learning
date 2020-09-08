08049ede0dc7b63ff8cbf0d63e5969c4504688d5
Cunliang_AHU <33220873+ahuzcl@users.noreply.github.com>
2020-09-08 09:37:27 +0800

    demo3-if-else

---
+++ src/com/cunliang/day01/Demo3.java
@@ -0,0 +1,22 @@
+package com.cunliang.day01;
+
+public class Demo3 {
+    public static void main(String[] args) {
+
+        boolean x = true;
+        boolean y = false;
+        short z = 40;
+        if ((z++==40) && (y=true)){
+            z++;
+        }
+        if ((x=false) || (++z==43)){
+            z++;
+        }
+
+        System.out.println("z = " + z);
+
+
+
+
+    }
+}
